d614a5af163fc05415ddfd41461429480dea9b4c
YahyaMohammadIbrahim <92376549+YahyaMohammadIbrahim@users.noreply.github.com>
2021-12-21 16:08:20 +0300

    Delete facebook-main directory

--- facebook-main/src/main/java/ibmtal/com/facebook/business/services/PageService.java
+++
@@ -1,9 +0,0 @@
-package ibmtal.com.facebook.business.services;
-
-import java.util.ArrayList;
-
-import ibmtal.com.facebook.entity.Page;
-
-public interface PageService {
-	ArrayList<Page> getAll();
-}
--- facebook-main/src/main/java/ibmtal/com/facebook/database/PageDao.java
+++
@@ -1,9 +0,0 @@
-package ibmtal.com.facebook.database;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ibmtal.com.facebook.entity.Page;
-
-public interface PageDao extends JpaRepository<Page, Integer>  {
-
-}
